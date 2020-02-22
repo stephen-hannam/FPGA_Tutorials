cab66d85d95d0e54bafaf8fa05a9de19b2394a61
srh <stephenrosshannam@gmail.com>
2020-02-23 07:44:31 +1100

    fixed up syntax and other minor errors, in process of constructing vivado project

--- onesReductionScan_FPGA/VHDL/arith_reduce_ones_vhdl.vhd
+++ onesReductionScan_FPGA/VHDL/arith_reduce_ones_vhdl.vhd
@@ -6,9 +6,9 @@ use work.helper_funcs.all;
 
 entity arith_reduce_ones_vhdl is
   generic(
-    INPUT_WIDTH : natural := 4,
-    INPUT_MSB   : natural := 3,
-    CNT_MSB     : natural := 2,
+    INPUT_WIDTH : natural := 4;
+    INPUT_MSB   : natural := 3;
+    CNT_MSB     : natural := 2;
     CUM_CNT_MSB : natural := (2+1)*4-1
   );
   port(
@@ -16,25 +16,29 @@ entity arith_reduce_ones_vhdl is
     cnt_out     : out std_logic_vector(CNT_MSB downto 0);
     cum_cnt_out : out std_logic_vector(CUM_CNT_MSB downto 0)
   );
-end arith_reduce_ones;
+end arith_reduce_ones_vhdl;
 
-architecture BEH of arith_reduce_ones is
+architecture BEH of arith_reduce_ones_vhdl is
   constant CUM_UPP : natural := CNT_MSB + 1;
 begin
   scan_reduce : process(arr_in)
     -- PLO: variables and use in loops + multi-driven nets
-    variable v_cnt : unsigned(CNT_MSB downto 0) := (others => '0');
+    variable v_cnt      : unsigned(CNT_MSB downto 0)     := (others => '0');
+    variable v_cum_cnt  : unsigned(CUM_CNT_MSB downto 0) := (others => '0');
   begin
     cnt_out     <= std_logic_vector(v_cnt);
+    cum_cnt_out <= std_logic_vector(v_cum_cnt);
     cum_cnt_out <= (others => '0');
     for i in natural range 0 to INPUT_MSB loop
-      v_cnt := v_cnt + arr_in(i);
+      if arr_in(i) = '1' then
+        v_cnt := v_cnt + 1;
+      end if;
       -- cnt_out  <= cnt_out + arr_in(i); -- why won't this work?
       if arr_in(i) = '1' then
         if i = 0 then
-          cum_cnt_out(0 to CUM_UPP) <= (CUM_UPP => '1', others => '0');
-        elsif then
-          cum_cnt_out(i*CUM_UPP to (i+1)*CUM_UPP) <= cum_cnt_out((i-1)*CUM_UPP to i*CUM_UPP) + 1;
+          v_cum_cnt(0 to CUM_UPP) := (CUM_UPP => '1', others => '0');
+        else
+          v_cum_cnt(i*CUM_UPP to (i+1)*CUM_UPP) := v_cum_cnt((i-1)*CUM_UPP to i*CUM_UPP) + 1;
         end if;
       end if;
     end loop;
